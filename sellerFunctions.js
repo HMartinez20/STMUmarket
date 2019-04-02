e23a1ea3718cad8372270eb2524a98366ae12d58
salvadoor <46605004+salvadoor@users.noreply.github.com>
2019-04-02 16:49:17 -0500

    Update sellerFunctions.js

--- sellerFunctions.js
+++ sellerFunctions.js
@@ -1,6 +1,6 @@
 /* JS used for seller.html */
 
-console.log("4:47");
+console.log("4:49");
 
 $( document ).ready(function() {	
 /* see if user is signed in */
@@ -49,9 +49,11 @@ $( document ).ready(function() {
 			/*sample for 1 folder files */
 			let ref = firebase.database().ref('items');
 			
-			ref.orderByChild('items/seller').equalTo('srodriguezgome').on("value", (snapshot) => {
+			ref.orderByChild('seller').equalTo('srodriguezgome').on("value", (snapshot) => {
 				console.log("Got snapshot");
 				console.log(snapshot.size);
+				var s = snapshot.size;
+				console.log(s);
 				
 			});
 	
