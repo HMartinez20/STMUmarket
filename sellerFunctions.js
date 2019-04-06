84cdc74340d7ad10e4714aa4dced765509b400d6
salvadoor <46605004+salvadoor@users.noreply.github.com>
2019-04-06 12:59:38 -0500

    Update sellerFunctions.js

--- sellerFunctions.js
+++ sellerFunctions.js
@@ -1,6 +1,6 @@
 /* JS used for seller.html */
 
-console.log("11:57");
+console.log("11:59");
 
 $( document ).ready(function() {
 /* see if user is signed in */
@@ -69,7 +69,7 @@ $( document ).ready(function() {
 					var post = doc.data();
 					var imgPath = "posts/" + post.image1;
 					/* listing's title and price in card body */
-					firebase.storage().ref("posts/1.jpg").getDownloadURL().then((url) => {
+					firebase.storage().ref('posts/1.jpg').getDownloadURL().then((url) => {
 					console.log(url);
 					var cardBody = "<div class='card-body'><h4 class='card-title'>"
 					cardBody+= post.title + "</h4><p class='card-text'> Asking Price: $" + post.price + "</p></div>";
