845fb20e6119e3f0c50f22a47dd531d507a76a8d
salvadoor <46605004+salvadoor@users.noreply.github.com>
2019-04-07 02:36:07 -0500

    Update sellerFunctions.js

--- sellerFunctions.js
+++ sellerFunctions.js
@@ -1,6 +1,6 @@
 /* JS used for seller.html */
 
-console.log("2:34");
+console.log("2:36");
 
 $( document ).ready(function() {
 /* see if user is signed in */
@@ -13,7 +13,7 @@ $( document ).ready(function() {
 
 	/* get UID passed in URL, url format is https://hmartinez20.github.io/STMUmarket/seller.html#"UIDgoesHere" */
 	var sellerId = window.location.hash.substring(1)
-	if (sellerID == null){
+	if (sellerId == null){
 		window.location.href = "https://hmartinez20.github.io/STMUmarket/";
 	}
 	
