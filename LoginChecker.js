afa5533eb6300d0a3eece2861122ea6a792e8a40
Tferenczi <47160852+Tferenczi@users.noreply.github.com>
2019-04-09 14:56:52 -0500

    Update LoginChecker.js

--- LoginChecker.js
+++ LoginChecker.js
@@ -1,1 +1,15 @@
 
+$( document ).ready(function() {
+  	firebase.auth().onAuthStateChanged(function(user) {
+  		if (user) {
+    		console.log('signed in');
+			document.getElementById("login").setAttribute("hidden","");
+        document.getElementById("signUp").setAttribute("hidden","");
+  		} else {
+        document.getElementById("myAccout").setAttribute("hidden","");
+        document.getElementById("signOut").setAttribute("hidden","");
+    		console.log(' not signed in');
+  		}
+		});
+	});
+
