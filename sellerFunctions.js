7f2d69cf9f672ba22754681af38f99c880f50ac9
salvadoor <46605004+salvadoor@users.noreply.github.com>
2019-04-09 10:30:51 -0500

    Update sellerFunctions.js

--- sellerFunctions.js
+++ sellerFunctions.js
@@ -1,5 +1,5 @@
 /* JS used for seller.html */
-console.log("3:48");
+console.log("10:30");
 
 /* get seller name that is passed through url */
 var sellerId = window.location.hash.substring(1)
@@ -82,7 +82,7 @@ $( document ).ready(function() {
 						var cardBody = "<div class='card-body'><h4 class='card-title'>"
 						cardBody+= post.title + "</h4><p class='card-text'> Asking Price: $" + post.price
 						cardBody+= "<br> <a href=" + postURL +">Go To Posting</a></p></div>";
-						var cardImage = "<img class='card-image-top mt-3' src=" + url + "alt=''></img>";
+						var cardImage = "<img class='card-image-top' src=" + url + "alt=''></img>";
 						card = document.createElement("div");
 						card.className = "card col-3";
 						card.style.cssText = 'max-width:40rem;'
