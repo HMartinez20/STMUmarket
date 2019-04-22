d9f613a4e08fc707f292d4c955b3f2479491c53f
jveracruz <jveracruz@mail.stmarytx.edu>
2019-04-22 14:31:45 -0500

    Update PasswordChecker.js
    Added comments to the code.

--- PasswordChecker.js
+++ PasswordChecker.js
@@ -1,3 +1,7 @@
+/* This Javascript function is checks to see if the user is typing the same password on both lines and lets the usesr know if they are 
+   typing it correctly or not.
+
+*/
 var check = function() {
   if (document.getElementById('pass').value ==
     document.getElementById('passwordRepeat').value) {
@@ -8,5 +12,5 @@ var check = function() {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'not matching';
   }
-  console.log("good");
+  console.log("good"); /* This is for error checking on the console tab when inspecting the website. */
 }
