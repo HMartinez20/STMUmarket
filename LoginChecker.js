dda34a7496acd35113aa1a67feb14391731aa89b
salvadoor <46605004+salvadoor@users.noreply.github.com>
2019-04-23 05:20:56 -0500

    Update LoginChecker.js
    comments

--- LoginChecker.js
+++ LoginChecker.js
@@ -1,14 +1,14 @@
- <!-- function to hide or show login, sign up, account, and sign out --> 
+ // function to hide or show login, sign up, account, and sign out on navbar
 
 firebase.auth().onAuthStateChanged(function(user) {
-	if (user) {
+	if (user) { // display account and sign out
 		document.getElementById("login").setAttribute("hidden","");
         	document.getElementById("signUp").setAttribute("hidden","");
 		document.getElementById("myAccount").removeAttribute("hidden");
         	document.getElementById("signOut").removeAttribute("hidden");
 		document.getElementById('myAccount').innerHTML = user.email;
 			//alert("Welcome: "+user.email);
-  	} else {
+  	} else { // display login and sign up
         	document.getElementById("myAccount").setAttribute("hidden","");
         	document.getElementById("signOut").setAttribute("hidden","");
 		document.getElementById("login").removeAttribute("hidden");
