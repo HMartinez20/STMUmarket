15b80851871e36c87fc625f41505e3582c3cbbbf
HMartinez20 <32554944+HMartinez20@users.noreply.github.com>
2019-04-30 11:44:06 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,7 +1,15 @@
-var category = "none";
+/* Javscript for index page
+   Generates the table to display all current, unsold items from
+	 our Firebase database.
+	 Last Updated: 4/30/2019 by Hailey Martinez
+ */
 
+var category = "none"; // On page load, change selected category to none
+
+// When the filter is changed, the table is cleared and
+// re-generated according to the new filter.
 function changeFilter(newFilter, newOrder){
-	console.log(newFilter+", "+newOrder)
+	//console.log(newFilter+", "+newOrder);
 	if(category != "none"){ genListings(category, newFilter, newOrder); }
 }
 
@@ -11,20 +19,17 @@ query = query.where(x, '==', y)
 query.get()...
 */
 
-// search listings and display based on category and filters
+// Search listings and display based on category and filters
+// On page load, set default search parameters
 function genListings(search = 'none', filter = 'price', order = 'asc'){
-	console.log(search+", "+filter+", "+order)
-	category = search;
+	//console.log(search+", "+filter+", "+order);
+	category = search; // Change category to selected category
 	if(search != 'none'){
-		document.getElementById("listings").innerHTML = '';
+		document.getElementById("listings").innerHTML = ''; // Clear table
 		db.collection("items").where("category", "==", search).orderBy(filter, order).get().then(function(querySnapshot){
-			querySnapshot.forEach(function(doc){ // doc.data() is never undefined for query doc snapshots
-				if(doc.data().sold == "no"){ // only show unsold listings
-					// create card for listing and append to the page
-// 					var placeholder = document.createElement("div");
-// 					placeholder.setAttribute("class","col-3");
-
-					var imgSrc = "{{site.baseurl}}/Empty.jpg";
+			querySnapshot.forEach(function(doc){
+				if(doc.data().sold == "no"){ // Only show unsold listings
+					var imgSrc = "{{site.baseurl}}/Empty.jpg"; // Default image
 					if(doc.data().image1){ imgSrc = doc.data().image1; }
 
 					var card = document.createElement("div");
@@ -33,19 +38,17 @@ function genListings(search = 'none', filter = 'price', order = 'asc'){
 					card.innerHTML += '<div class="card-body pl-0 pb-0 pr-0"><p class="text-truncate card-title"><a href="' + ('item.html#' + doc.id) + '" target="_blank">'+ doc.data().title +'</a></p><p class="card-text">$'+ doc.data().price;
 					card.innerHTML += '</p></div>';
 
-// 					placeholder.appendChild(card);
 					document.getElementById("listings").appendChild(card);
-				} // end of if
+				}
 			});
 		});
 
+		// Show the appropriate category "badge" as confirmation of category change
 		var badgeArray = ["books","clothing","electronics","furniture","other"];
 		badgeArray.forEach(function(item){
 			 document.getElementById(item+"Badge").setAttribute("hidden","");
 		});
-
 		var badge = document.getElementById(search+"Badge");
 		if(badge.hasAttribute("hidden")){ badge.removeAttribute("hidden"); }
-		//     else{ badge.setAttribute("hidden",""); }
 	}
 }
