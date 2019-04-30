d7e1507c13ff9e8676f1beaae6af41bc2eceffea
Tferenczi <47160852+Tferenczi@users.noreply.github.com>
2019-04-30 11:51:19 -0500

    Update LoginChecker.js

--- LoginChecker.js
+++ LoginChecker.js
@@ -1,14 +1,15 @@
  // function to hide or show login, sign up, account, and sign out on navbar
+// last edit by tamas 4/22/19 
 
 firebase.auth().onAuthStateChanged(function(user) {
-	if (user) { // display account and sign out
+	if (user) { // display account and sign out button
 		document.getElementById("login").setAttribute("hidden","");
         	document.getElementById("signUp").setAttribute("hidden","");
 		document.getElementById("myAccount").removeAttribute("hidden");
         	document.getElementById("signOut").removeAttribute("hidden");
 		document.getElementById('myAccount').innerHTML = user.email;
 			//alert("Welcome: "+user.email);
-  	} else { // display login and sign up
+  	} else { // display login and sign up button
         	document.getElementById("myAccount").setAttribute("hidden","");
         	document.getElementById("signOut").setAttribute("hidden","");
 		document.getElementById("login").removeAttribute("hidden");
