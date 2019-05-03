3f23238e29acbb394c0250f2e06dedd9d5172e28
HMartinez20 <32554944+HMartinez20@users.noreply.github.com>
2019-05-03 16:45:16 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -21,7 +21,7 @@ function genListings(search = 'none', filter = 'price', order = 'asc'){
 	category = search; // Change category to selected category
 	if(search != 'none'){
 		document.getElementById("listings").innerHTML = '';
-		var query = db.collection("items").where("category", "==", search).orderBy(filter, price);
+		var query = db.collection("items").where("category", "==", search).orderBy(filter, order);
 		query.get().then(function(querySnapshot){
 			querySnapshot.forEach(function(doc){
 				if(doc.data().sold == "no"){ // Only show unsold listings
