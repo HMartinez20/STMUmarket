7a0e899a78278b8e9e47f06a8521c566e08387ea
HMartinez20 <32554944+HMartinez20@users.noreply.github.com>
2019-05-03 13:20:05 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -76,7 +76,7 @@ function genPage(pgNo, setStart, search, filter, order){
 	// TEST QUERY
 	var x = document.getElementById("myAccount").innerHTML;
 	if(x == 'hmartinez21@mail.stmarytx.edu' && search == "books"){
-		for(var i = 1; i <= 27; i++){
+		for(var i = 1; i <= 23; i++){
 			var query = db.collection("items").where("category", "==", search).orderBy(filter, order);
 			query.limit(4).startAt(i).get().then(function(querySnapshot){
 				querySnapshot.forEach(function(doc){
